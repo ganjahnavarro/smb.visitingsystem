a6c13dc4edabdbe4ca0370aaf73dd6eb822d47b6
Ganjah <ganjaboi.navarro@gmail.com>
2016-02-14 13:43:44 +0800

    added disclaimer

---
+++ disclaimer.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<?php include( $_SERVER['DOCUMENT_ROOT'] . '/includes/header.php' ); ?>
+</head>
+<body class="bg-primary">
+	<br /> <br /> <br />
+	
+	<div class="col-md-6 col-md-offset-3">
+		<img src="/resources/images/logo.png" class="img-responsive center-block" alt="Responsive image"> <br /> <br />
+	</div>
+	
+	<div id="disclaimer" class="col-md-6 col-md-offset-3">
+		<p class='text-warning'><strong>No warranties</strong></p>
+		<p>This website is provided “as is” without any representations or warranties, express or implied.  San Miguel Virtual Lobby makes no representations or warranties in relation to this website or the information and materials provided on this website.</p>  
+		<p>Without prejudice to the generality of the foregoing paragraph, San Miguel Virtual Lobby does not warrant that:</p>
+			
+		<p>- This website will be constantly available, or available at all; or</p>
+		<p>- The information on this website is complete, true, accurate or non-misleading.</p>
+			
+		<p>Nothing on this website constitutes, or is meant to constitute, advice of any kind.</p>
+        <br/>
+        
+		<p class='text-warning'><strong>Limitations of liability</strong></p>
+		<p>San Miguel Virtual Lobby will not be liable to you (whether under the law of contract, the law of torts or otherwise) in relation to the contents of, or use of, or otherwise in connection with, this website:</p>
+        
+        <p>- To the extent that the website is provided free-of-charge, for any direct loss; or</p>
+        <p>- For any indirect, special or consequential loss; or</p>
+        <p>- For any business losses, loss of revenue, income, profits or anticipated savings, loss of contracts or business relationships, loss of reputation or goodwill, or loss or corruption of information or data.</p>
+        
+        <p>These limitations of liability apply even if San Miguel Virtual Lobby has been expressly advised of the potential loss.</p>
+        <br />
+        
+        <p class='text-warning'><strong>Exceptions</strong></p>        
+        <p>Nothing in this website disclaimer will exclude or limit any warranty implied by law that it would be unlawful to exclude or limit; and nothing in this website disclaimer will exclude or limit San Miguel Virtual Lobby's liability in respect of any:</p>
+        
+        <p>- Death or personal injury caused by San Miguel Virtual Lobby's negligence;</p>
+        <p>- Fraud or fraudulent misrepresentation on the part of San Miguel Virtual Lobby; or</p>
+        <p>- Matter which it would be illegal or unlawful for San Miguel Virtual Lobby to exclude or limit, or to attempt or purport to exclude or limit, its liability.</p>
+        <br />
+        
+        <p class='text-warning'><strong>Reasonableness</strong></p>        
+        <p>By using this website, you agree that the exclusions and limitations of liability set out in this website disclaimer are reasonable. </p>
+        <p>If you do not think they are reasonable, you must not use this website.</p>
+        <br />
+        
+        <p class='text-warning'><strong>Other parties</strong></p>   
+        <p>You accept that, as a limited liability entity, San Miguel Virtual Lobby has an interest in limiting the personal liability of its officers and employees.</p>
+        <p>You agree that you will not bring any claim personally against San Miguel Virtual Lobby's officers or employees in respect of any losses you suffer in connection with the website.</p>
+        <p>Without prejudice to the foregoing paragraph, you agree that the limitations of warranties and liability set out in this website disclaimer will protect San Miguel Virtual Lobby's officers, employees, agents, subsidiaries, successors, assigns and sub-contractors as well as San Miguel Virtual Lobby.</p>
+        <br />
+        
+        <p class='text-warning'><strong>Unenforceable provisions</strong></p>   
+        <p>If any provision of this website disclaimer is, or is found to be, unenforceable under applicable law, that will not affect the enforceability of the other provisions of this website disclaimer.</p>
+        
+        <br/>
+        <br/>
+	</div>
+
+</body>
+</html>
